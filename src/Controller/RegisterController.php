00212d3351739bf97207ab435d205b6545d6aea1
timr <timrcode0@gmail.com>
2024-01-12 14:56:24 +0100

    hash pwd

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -4,20 +4,50 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegisterType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RegisterController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
     #[Route('/inscription', name: 'register')]
-    public function index(): Response
+    public function index(Request $request, UserPasswordHasherInterface $passwordHasher): Response
     {
         $user = new User();
+        
+
         $form = $this->createForm(RegisterType::class, $user);
 
-        return $this->render('register/index.html.twig',[
-            'form' => $form->createView()
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+    
+            $plaintextPassword = $user->getPassword();
+            $hashedPassword = $passwordHasher->hashPassword(
+                $user,
+                $plaintextPassword
+            );
+            $user->setPassword($hashedPassword);
+    
+
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
+
+           
+            // Traitement du nouvel utilisateur (redirection, message flash, etc.)
+        }
+
+        return $this->render('register/index.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
