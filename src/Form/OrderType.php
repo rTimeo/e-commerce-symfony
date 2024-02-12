dcd3c11c6ec053108cab4067814e676d0d9d327c
rTimeo <timrcode0@gmail.com>
2024-02-12 22:07:48 +0100

    add form order

---
+++ src/Form/OrderType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Address;
+use App\Entity\Carrier;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class OrderType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $user = $options['user'];
+        $builder
+            ->add('adresses', EntityType::class, [
+                'choices' => $user->getAddresses(),
+
+                'label'=> 'choisissez votre adresse de livraison',
+                'required'=>true,
+                'class' => Address::class,
+                'multiple'=> false,
+                'expanded' => true,
+            ])
+        
+        ->add('carriers', EntityType::class, [
+
+            'label'=> 'choisissez votre adresse de livraison',
+            'required'=>true,
+            'class' => Carrier::class,
+            'multiple'=> false,
+            'expanded' => true,
+        ])
+        ->add('submit', SubmitType::class,[
+            'label'=>'valider ma commander'
+        ])
+    ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'user' => array()
+        ]); 
+    }
+}
